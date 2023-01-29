084509e54be20411877042d6a61b15169deca391
Kaiji21 <118065177+Kaiji21@users.noreply.github.com>
2023-01-29 22:30:52 +0100

    Initial commit

---
+++ Task_to_teammember_randomly.py
@@ -0,0 +1,18 @@
+
+import random
+
+team_members = ['Hakim', 'Farid', 'Asmae', 'Amina', 'Oumaima']
+taches = ['centre_gestionnaire_formController', 'contactrespo', 'dashbord_controller', 'Historique_C_R_O', 'historique_formController'
+, 'historique_respo_equi', 'historiques_salles_respo', 'reservation_controller']
+taqsimhh = {member: [] for member in team_members}
+
+while len(taches) > 0:
+    for member in team_members:
+        if len(taches) > 0:
+            task_to_assign = random.choice(taches)
+            taqsimhh[member].append(task_to_assign)
+            taches.remove(task_to_assign)
+
+print("Taqsim dyal les taches:")
+for member in team_members:
+    print(member, ":", taqsimhh[member])
